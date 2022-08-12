9174a22ca942dd6dd439a4625a3c1b310f27a122
Neyber Rojas <nrojas@pros.com>
2022-08-11 20:14:10 -0400

    Adding the tricks options

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write("HELLO FROM THE MOJIX BOOTCAMP")
+st.title("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
+
+option = st.selectbox(
+     'Select a python trick that you want to know',
+     ('Walrus operator', 'Splitting a string', 'Reversing a string', 'Merging two dictionaries', 'The zip() function', 'Assigning multiple list values to a variable', 'Lambda function', 'Swapping variable value', 'Use a password in your code')
+     )
+
+st.write('You selected:', option)

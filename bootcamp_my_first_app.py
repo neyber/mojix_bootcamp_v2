1dc02cd6668506cd8d415a3b4db3b6bac0db1a90
Neyber Rojas <nrojas@pros.com>
2022-08-11 20:21:02 -0400

    Adding logic of Walrus operator

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -7,4 +7,18 @@ option = st.selectbox(
      ('Walrus operator', 'Splitting a string', 'Reversing a string', 'Merging two dictionaries', 'The zip() function', 'Assigning multiple list values to a variable', 'Lambda function', 'Swapping variable value', 'Use a password in your code')
      )
 
-st.write('You selected:', option)
+if option == 'Walrus operator':
+     st.write('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
+     st.header('Example')
+     
+     code = '''
+          Mylist = [1,2,3]
+          if(l := len(mylist) > 2)
+          print(l)
+     '''
+
+     st.code(code, language='python')
+     
+     st.header('Output')
+
+     st.code('3', language='python')
